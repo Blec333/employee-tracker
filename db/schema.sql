c9c65b1e3e2e4889e8d70574fc6d442771d9321b
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-04-30 21:50:32 -0700

    add schema create dept table

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 USE company_db;
+
+
+CREATE TABLE departments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  department VARCHAR(30) NOT NULL
+);
