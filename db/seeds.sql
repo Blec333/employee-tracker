072c7d3bdc115bd5fa7d5d58eb3af316aa1f3039
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-04-30 21:57:38 -0700

    add roles table seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,12 @@ VALUES ("Engineering"),
        ("Sales");
 
        
+INSERT INTO roles (title, department_id, salary)
+VALUES ("Sales Lead", 4, "100000"),
+       ("Salesperson", 4, "80000"),
+       ("Lead Engineer", 1, "150000"),
+       ("Software Engineer", 1, "120000"),
+       ("Account Manager", 2, "160000"),
+       ("Accountant", 2, "125000"),
+       ("Legal Team Lead", 3, "250000"),
+       ("Lawyer", 3, "190000");
