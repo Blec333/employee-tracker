d7d8bbf47a6509be1b3dd2a3e2236030083d1c13
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-04-30 21:52:51 -0700

    add schema create roles table

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,12 @@ CREATE TABLE departments (
   id INT AUTO_INCREMENT PRIMARY KEY,
   department VARCHAR(30) NOT NULL
 );
+
+
+CREATE TABLE roles (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  department_id INT,
+  salary VARCHAR(9) NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
