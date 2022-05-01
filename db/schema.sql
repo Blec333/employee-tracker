b02bff223c5aee267706135d01425aed8779c6e4
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-04-30 21:53:15 -0700

    add schema create employees table

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,12 @@ CREATE TABLE roles (
   salary VARCHAR(9) NOT NULL,
   FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
+
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  firstName VARCHAR(100) NOT NULL,
+  lastName VARCHAR(100),
+  role_id INT,
+  manager VARCHAR(30),
+  FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+);
