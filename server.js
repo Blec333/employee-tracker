9fa61d3227b0324085a53d38e92e71865b3ded57
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-05-02 21:19:34 -0700

    add view function concatenation of headers

--- server.js
+++ server.js
@@ -40,10 +40,7 @@ app.listen(PORT, () => {
 
 //==================================================================================================================
 //PRIMARY FUNCTION - MAIN MENU======================================================================================
-//==================================================================================================================
-
 function mainMenu() {
-  console.log(` `);
   const questions = [
     inquirer
       .prompt([
@@ -59,25 +56,34 @@ function mainMenu() {
             'Add Role',
             'Add Employee',
             'Update Employee',
+            'Delete Department',
+            'Delete Employee',
+            'Delete Role',
             'Quit'
           ]
         }
       ])
       .then((response) => {
-        if (response.nextAction === 'Update Employee') {
-          updateEmployee(true);
+        if (response.nextAction === 'View All Departments') {
+          viewDepartments(true);
         } else if (response.nextAction === 'View All Roles') {
           viewRoles(true);
-        } else if (response.nextAction === 'Add Role') {
-          addRole(true);
-        } else if (response.nextAction === 'View All Departments') {
-          viewDepartments(true);
-        } else if (response.nextAction === 'Add Department') {
-          addDepartment(true);
         } else if (response.nextAction === 'View All Employees') {
           viewEmployees(true);
+        } else if (response.nextAction === 'Add Department') {
+          addDepartment(true);
+        } else if (response.nextAction === 'Add Role') {
+          addRole(true);
         } else if (response.nextAction === 'Add Employee') {
           addEmployee(true);
+        } else if (response.nextAction === 'Update Employee') {
+          updateEmployee(true);
+        } else if (response.nextAction === 'Delete Department') {
+          deleteDepartment(true);
+        } else if (response.nextAction === 'Delete Employee') {
+          deleteEmployee(true);
+        } else if (response.nextAction === 'Delete Role') {
+          deleteRole(true);
         } else if (response.nextAction === 'Quit') {
           quitApp(false);
         } else {
@@ -90,55 +96,55 @@ function mainMenu() {
 
 // Quit the app message
 function quitApp() {
-  return new Promise((resolve, reject) => {
-    db.end((err) => {
-     if (err) {
-      console.error('error during disconnection', err.stack)
-      return reject(err)
-     }
-     console.log('db has disconnected')
-     return resolve()
-    })
-});
+  process.exit();
 }
+//PRIMARY FUNCTION - MAIN MENU======================================================================================
+//==================================================================================================================
+
+
 
 //==================================================================================================================
 //VIEW THINGS=======================================================================================================
-//==================================================================================================================
-
 // -- WHEN I choose to view all departments
 // -- THEN I am presented with a formatted table showing department names and department ids
 function viewDepartments(menuStatus) {
-  return new Promise((resolve, reject) => {
-    console.log(` `);
-    db.query('SELECT * FROM departments', function (err, results) {
-      resolve(console.table(results));
-      console.log(` `);
-      if (menuStatus === true) { mainMenu(); }
-    });
+  db.query('SELECT * FROM departments', function (err, results) {
+    console.table(results);
+    mainMenu();
   });
 }
 
 // -- WHEN I choose to view all roles
 // -- THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 function viewRoles(menuStatus) {
-  console.log(` `);
-  db.query('SELECT r.id, title, d.department, r.salary FROM roles AS r INNER JOIN departments AS d ON r.department_id = d.id', function (err, results) {
+  db.query(`SELECT r.id, 
+  title, 
+  concat(d.id, ' - ', d.department) AS department, 
+  r.salary 
+  FROM roles r 
+  LEFT JOIN departments d ON r.department_id = d.id`, function (err, results) {
     console.table(results);
-    console.log(` `);
-    if (menuStatus === true) { mainMenu(); }
+    mainMenu();
   });
 }
 
 // -- WHEN I choose to view all employees
-
 // -- THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 function viewEmployees(menuStatus) {
-  console.log(` `);
-  db.query(`SELECT e.id, e.firstName, e.lastName, r.title, d.department, r.salary, concat(m.firstName, ' ', m.lastName) AS managers FROM employees e LEFT JOIN employees m ON e.manager_id = m.id LEFT JOIN roles r ON e.role_id = r.id LEFT JOIN departments d ON r.department_id = d.id`, function (err, results) {
+  db.query(`SELECT 
+  e.id, 
+  e.firstName, 
+  e.lastName, 
+  concat(r.id, ' - ', r.title) AS title, 
+  concat(d.id, ' - ', d.department) AS department, 
+  r.salary, 
+  concat(m.id, ' - ', m.firstName, ' ', m.lastName) AS managers 
+  FROM employees e 
+  LEFT JOIN employees m ON e.manager_id = m.id 
+  LEFT JOIN roles r ON e.role_id = r.id 
+  LEFT JOIN departments d ON r.department_id = d.id`, function (err, results) {
     console.table(results);
-    console.log(` `);
-    if (menuStatus === true) { mainMenu(); }
+    mainMenu();
   });
 }
 
@@ -147,22 +153,9 @@ function viewManagers(menuStatus) {
   console.log(`Managers: `);
   db.query('SELECT id, firstName, lastName FROM employees WHERE manager_id IS NULL', function (err, results) {
     console.table(results);
-    console.log(` `);
-    if (menuStatus === true) { mainMenu(); }
+    mainMenu();
   });
 }
-
-// View employee ids and names in one column function
-function viewEmployeeIDsNames(menuStatus) {
-  console.log(`Managers: `);
-  db.query(`SELECT concat(id,' ', firstName,' ', lastName) AS employees FROM employees`, function (err, results) {
-    console.table(results);
-    console.log(` `);
-    if (menuStatus === true) { mainMenu(); }
-  });
-}
-
-//==================================================================================================================
 //VIEW THINGS=======================================================================================================
 //==================================================================================================================
 
@@ -171,11 +164,8 @@ function viewEmployeeIDsNames(menuStatus) {
 
 //==================================================================================================================
 //ADD THINGS========================================================================================================
-//==================================================================================================================
-
 // -- WHEN I choose to add a department
 // -- THEN I am prompted to enter the name of the department and that department is added to the database
-
 async function addDepartment(menuStatus) {
   const questions = [
     inquirer
@@ -187,21 +177,16 @@ async function addDepartment(menuStatus) {
         }
       ])
       .then((response) => {
-        console.log(` `);
         if (response.name) {
           db.query(`INSERT INTO departments (department) VALUES ("${response.name}")`, function (err, results) {
-            console.log(` `);
           });
         } else {
           console.log(`Provided information was incomplete, please try again`);
           console.log(`Entered department name was: ${response.name}`);
-          console.log(` `);
-          if (menuStatus === true) { mainMenu(); }
         }
-      }).then((response) => {
-        viewDepartments();
-        console.log(` `);
-        if (menuStatus === true) { mainMenu(); }
+      })
+      .then((response) => {
+        mainMenu();
       })
   ];
 }
@@ -232,20 +217,16 @@ function addRole(menuStatus) {
         if (response.title && response.department_id && response.salary) {
           console.log(` `);
           db.query(`INSERT INTO roles (title, department_id, salary) VALUES ("${response.title}", ${response.department_id}, "${response.salary}")`, function (err, results) {
-            console.log(` `);
           });
         } else {
           console.log(`Provided information was incomplete, please try again`);
           console.log(`Entered title was: ${response.title}`);
           console.log(`Entered department_id was: ${response.department_id}`);
           console.log(`Entered salary was: ${response.salary}`);
-          console.log(` `);
-          if (menuStatus === true) { mainMenu(); }
+          mainMenu();
         }
       }).then((response) => {
-        viewRoles();
-        console.log(` `);
-        if (menuStatus === true) { mainMenu(); }
+        mainMenu();
       })
   ];
 }
@@ -278,10 +259,8 @@ function addEmployee(menuStatus) {
         }
       ])
       .then((response) => {
-        if (response.firstName && response.lastName && response.role_id && response.manager) {
-          console.log(` `);
-          db.query(`INSERT INTO employees (firstName, lastName, role_id, manager) VALUES ("${response.firstName}", "${response.lastName}", ${response.role_id}, ${response.manager_id})`, function (err, results) {
-            console.log(` `);
+        if (response.firstName && response.lastName && response.role_id && response.manager_id) {
+          db.query(`INSERT INTO employees (firstName, lastName, role_id, manager_id) VALUES ("${response.firstName}", "${response.lastName}", ${response.role_id}, ${response.manager_id})`, function (err, results) {
           });
         } else {
           console.log(`Provided information was incomplete, please try again`);
@@ -289,18 +268,12 @@ function addEmployee(menuStatus) {
           console.log(`Entered last name was: ${response.lastName}`);
           console.log(`Entered role ID was: ${response.role_id}`);
           console.log(`Entered manager was: ${response.manager_id}`);
-          console.log(` `);
         }
       }).then((response) => {
-        viewEmployees();
-        console.log(` `);
-      }).then((response) => {
-        if (menuStatus === true) { mainMenu(); }
+        mainMenu();
       })
   ];
 }
-
-//==================================================================================================================
 //ADD THINGS========================================================================================================
 //==================================================================================================================
 
@@ -309,8 +282,6 @@ function addEmployee(menuStatus) {
 
 //==================================================================================================================
 //UPDATE THINGS=====================================================================================================
-//==================================================================================================================
-
 // -- WHEN I choose to update an employee role
 // -- THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
 function updateEmployee(menuStatus) {
@@ -344,36 +315,32 @@ function updateEmployee(menuStatus) {
         }
       ])
       .then((response) => {
-        console.log(response);
         if (response.firstName !== '') {
           db.query(`UPDATE employees SET firstName = '${response.firstName}' WHERE id = ?`, response.id, function (err, results) {
             console.log(` `);
           });
         }
         if (response.lastName !== '') {
-          db.query(`UPDATE employees SET firstName = '${response.lastName}' WHERE id = ?`, response.id, function (err, results) {
+          db.query(`UPDATE employees SET lastName = '${response.lastName}' WHERE id = ?`, response.id, function (err, results) {
             console.log(` `);
           });
         }
         if (response.role_id !== '') {
-          db.query(`UPDATE employees SET firstName = ${response.role_id} WHERE id = ?`, response.id, function (err, results) {
+          db.query(`UPDATE employees SET role_id = ${response.role_id} WHERE id = ?`, response.id, function (err, results) {
             console.log(` `);
           });
         }
         if (response.manager_id !== '') {
-          db.query(`UPDATE employees SET firstName = ${response.manager_id} WHERE id = ?`, response.id, function (err, results) {
+          db.query(`UPDATE employees SET manager_id = ${response.manager_id} WHERE id = ?`, response.id, function (err, results) {
             console.log(` `);
           });
         }
       })
       .then((response) => {
-        console.log(` `);
-        if (menuStatus === true) { mainMenu(); }
+        mainMenu();
       })
   ];
 }
-
-//==================================================================================================================
 //UPDATE THINGS=====================================================================================================
 //==================================================================================================================
 
@@ -381,48 +348,80 @@ function updateEmployee(menuStatus) {
 
 
 
-
-
-
-// db.query("DELETE FROM favorite_books WHERE id = ?", deletedRow, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-function prompt() {
+//==================================================================================================================
+//DELETE THINGS=====================================================================================================
+function deleteEmployee(menuStatus) {
   const questions = [
     inquirer
       .prompt([
         {
           type: 'input',
-          message: 'What is ?',
-          name: 'name'
+          message: 'Which employee would you like to delete? (Enter ID)',
+          name: 'id'
         }
       ])
       .then((response) => {
-
+        if (response.firstName !== '') {
+          db.query(`DELETE FROM employees WHERE id = ?`, response.id, function (err, results) {
+            console.log(` `);
+          });
+        }
       })
       .then((response) => {
-        
+        mainMenu();
+      })
+  ];
+}
+
+function deleteRole(menuStatus) {
+  const questions = [
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: 'Which role would you like to delete? (Enter ID)',
+          name: 'id'
+        }
+      ])
+      .then((response) => {
+        if (response.firstName !== '') {
+          db.query(`DELETE FROM roles WHERE id = ?`, response.id, function (err, results) {
+            console.log(` `);
+          });
+        }
+      })
+      .then((response) => {
+        mainMenu();
+      })
+  ];
+}
+
+function deleteDepartment(menuStatus) {
+  const questions = [
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: 'Which department would you like to delete? (Enter ID)',
+          name: 'id'
+        }
+      ])
+      .then((response) => {
+        if (response.firstName !== '') {
+          db.query(`DELETE FROM departments WHERE id = ?`, response.id, function (err, results) {
+            console.log(` `);
+          });
+        }
+      })
+      .then((response) => {
+        mainMenu();
       })
   ];
 }
 
 
+//DELETE THINGS=====================================================================================================
+//==================================================================================================================
 
 function init() {
   console.log(`                                    `);
@@ -438,26 +437,8 @@ function init() {
   console.log(` |_|_|_|__,|_|_|__,|_  |___|_|      `);
   console.log(`                   |___|            `);
   console.log(`                                    `);
-  new Promise((resolve, reject) => {
-    db.query('SOURCE ./db/schema.sql', function (err, results) {
-      resolve(console.log(''));
-    });
-  })
-    .then((response) => {
-      new Promise((resolve, reject) => {
-        db.query('SOURCE ./db/seeds.sql', function (err, results) {
-          resolve(console.log(''));
-        });
-      })
-        .then((response) => {
-          mainMenu();
-        })
-        .catch((err) => {
-          console.log('error thrown');
-          console.log(err.message);
-        });
-      });
-  }
+  mainMenu();
+}
 
 init();
 
